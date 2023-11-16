c32d55f0aa52b7d2bc245e148979e1cbb0d95f92
KristiyanAleksiev <71889022+ChrisX21@users.noreply.github.com>
2023-11-16 11:31:01 +0200

    Delete WebServer directory

--- WebServer/WebServer/HTTP/ContentType.cs
+++
@@ -1,8 +0,0 @@
-namespace WebServer.HTTP;
-
-public class ContentType
-{
-    public const string PlainText = "text/plain; charset=UTF-8";
-    public const string Html = "text/html;  charset=UTF-8";
-    
-}
