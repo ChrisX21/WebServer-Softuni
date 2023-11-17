fe4f8de3f1f4a76aa5a50c7da392087f63dee44a
ChrisX21 <chrisalexiev@icloud.com>
2023-11-18 00:55:47 +0200

    started session

---
+++ Web-Server/BasicWebServer.Server/HTTP/Session.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using BasicWebServer.Server.Common;
+
+namespace BasicWebServer.Server.HTTP;
+
+public class Session
+{
+    public const string SessionCookieName = "MyWebServerSID";
+    public const string SessionCookieDateKey = "CurrentDate";
+    private Dictionary<string, string> data;
+
+    public Session(string id)
+    {
+        Guard.AgainstNull(id, nameof(id));
+
+        this.Id = id;
+        this.data = new Dictionary<string, string>();
+    }
+    
+    public string Id { get; init; }
+
+    public string this[string key]
+    {
+        get => this.data[key];
+        set => this.data[key] = value;
+    }
+
+    public bool Containskey(string key) => this.data.ContainsKey(key);
+}
